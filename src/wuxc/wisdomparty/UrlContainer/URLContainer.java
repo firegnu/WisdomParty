537e37fd8e5bbcc363f697f5a99587c0592706d2
wuxc0204 <944672412@qq.com>
2017-03-12 19:50:04 +0800

    Url

---
+++ src/wuxc/wisdomparty/UrlContainer/URLContainer.java
@@ -0,0 +1,97 @@
+package wuxc.wisdomparty.UrlContainer;
+
+public class URLContainer {
+	// 域名
+	public static String MainUrl = "http://dev.huilansoft.cn";
+	// 获取文件
+	public static String GetFile = "/upload";
+	// 用户登录
+	public static String LoginIn = "api/member/login";
+	// 用户退出
+	public static String LoginOut = "api/member/logout";
+	// 城市数据
+	public static String GetCityData = "api/common/getCityData";
+	// 获取用户信息
+	public static String GetUserInfo = "api/member/getUserInfo";
+	// 上传用户头像
+	public static String UpLoadSignle = "uploadUserPortrait/uploadSignle";
+	// 获取用户消息列表
+	public static String GetListJsonData = "api/pubshare/messageNotice/getListJsonData";
+	// 获取消息
+	public static String GetJsonData = "api/pubshare/messageNotice/getJsonData";
+	// 获取app版本
+	public static String GetLatestVersion = "api/pubshare/sysVersion/getLatestVersion";
+	// 数据字典
+	public static String GetAllDicData = "api/common/getAllDicData";
+	// 组织机构
+	public static String GetAllOrg = "api/common/getAllOrg";
+	// 首页图片
+	public static String GetDisplayList = "api/pubshare/homeImage/getDisplayList";
+	// 查询党费
+	public static String QueryFare = "api/pb/common/queryFare";
+	// 获取积分、爱心基金、未读已读消息等
+	public static String GetMemberData = "api/pb/common/getMemberData";
+	// 我收到的消息
+	public static String GetListJsonData_2 = "api/console/systemMessage/getListJsonData";
+	// 消息查询
+	public static String GetJsonData_2 = "api/console/systemMessage/getJsonData";
+	// 消息统计
+	public static String GetListJsonStaticsData = "api/console/systemMessage/getListJsonStaticsData";
+	// 按照消息已读和未读进行数量统计
+	public static String GetListJsonStaticsDataByReadState = "api/console/systemMessage/getListJsonStaticsDataByReadState";
+	// 我发出的消息
+	public static String GetListJsonData_3 = "api/console/systemMessage/getListJsonData";
+	// 发送消息
+	public static String SendMessage = "api/console/systemMessage/sendMessage";
+	// 我的支部信息
+	public static String GetDeptInfo = "api/pb/deptpic/getDeptInfo";
+	// 党费缴纳记录
+	public static String partyFareRecordGetListJsonData = "api/pb/partyFareRecord/getListJsonData";
+	// 关系转接记录
+	public static String relationChangeGetListJsonData = "api/pb/relationChange/getListJsonData";
+	// 我的奖励、惩罚
+	public static String rewardPunishmentGetListJsonData = "api/pb/rewardPunishment/getListJsonData";
+	// 活动审批
+	public static String kndysqGetListJsonData = "api/pb/kndysq/getListJsonData";
+	// 活动数量统计
+	public static String kndysqGetListJsonStaticsData = "api/pb/kndysq/getListJsonStaticsData";
+	// 在线入党审批
+	public static String zxrdGetListJsonData = "api/pb/zxrd/getListJsonData";
+	// 积分兑换爱心基金
+	public static String convertScore = "api/pb/loveRecord/convertScore";
+	// 积分查询
+	public static String userScoregetListJsonData = "api/console/userScore/getListJsonData";
+	// 积分记录数量统计
+	public static String GetListJsonStaticsDataRecord = "api/console/userScore/api/console/userScore/getListJsonStaticsDataRecord";
+	// 爱心基金记录统计
+	public static String loveRecordGetListJsonStaticsData = "api/pb/loveRecord/getListJsonStaticsData";
+	// 爱心基金记录查询
+	public static String loveRecordGetListJsonData = "api/pb/loveRecord/getListJsonData";
+	// 我的评价
+	public static String partyEvaluationGetListJsonData = "api/pb/partyEvaluation/getListJsonData";
+	// 我的收藏
+	public static String myCollectionGetListJsonData = "api/pubshare/myCollection/getListJsonData";
+	// 删除收藏
+	public static String myCollectiondelete = "api/pubshare/myCollection/delete";
+	// 我的评论
+	public static String myCommentGetListJsonData = "api/pubshare/myComment/getListJsonData";
+	// 删除评论
+	public static String myCommentdelete = "api/pubshare/myComment/delete";
+	// 我的活动
+	public static String kndysqGetListJsonDataAct = "api/pb/kndysq/getListJsonDataAct";
+	// 我的活动记录数量统计
+	public static String GetListJsonStaticsDataAct = "api/pb/kndysq/getListJsonStaticsDataAct";
+	// 我的选举
+	public static String GetListJsonDataMyElection = "api/pb/election/getListJsonDataMyElection";
+	// 我的投票
+	public static String GetListJsonDataMyVote = "api/cms/voteRecord/getListJsonDataMyVote";
+	// 获取在线入党数据
+	public static String zxrdGetModelData = "api/pb/zxrd/getModelData";
+	// 在线入党图片上传
+	public static String formfileUploadUpLoadSignle = "form/formfileUpload/uploadSignle";
+	// 在线入党文件上传
+	public static String formfileUploadUpLoadSignle_2 = "form/formfileUpload/uploadSignle";
+	// 在线入党数据保存
+	public static String zxrdsaveData = "api/pb/zxrd/saveData";
+
+}
